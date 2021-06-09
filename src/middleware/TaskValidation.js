780f289ee940886230735dfe2aab825fefcf884b
Rafael Hayashi <rafaelhayashi8@gmail.com>
2021-06-09 18:58:57 -0300

    Middleware e validaçao de tarefa

---
+++ src/middleware/TaskValidation.js
@@ -0,0 +1,41 @@
+const TaskModel = require('../model/TaskModel')
+const { isPast } = require('date-fns')
+
+const TaskValidation = async(req, res, next) => {
+    const {macAddress, type, title, description, when} = req.body;
+
+    if(!macAddress) {
+      return res.status(400).json({error: 'macAddress é obrigatório'})  
+    }
+    else if(!type) {
+        return res.status(400).json({error: 'Tipo é obrigatório'})  
+      }
+      else if(!title) {
+        return res.status(400).json({error: 'Título é obrigatório'})  
+      }
+        else if(!description) {
+            return res.status(400).json({error: 'Descrição é obrigatoria'})  
+        }
+        else if(!when) {
+            return res.status(400).json({error: 'Data e Hora são obrigatórios'})  
+        }
+        else if(isPast(new Date(when))) {
+            return res.status(400).json({error: 'Escolha uma data e hora futura'})  
+        }
+    
+    else {
+        let exists;
+        exists = await TaskModel.findOne({
+            'when': {'$eq': new Date(when)},
+            'macAddress': {'$in': macAddress}
+        });
+        if(exists) {
+            return res.status(400).json({error: 'Já existe uma tarefa nesse dia e hora'})
+        }
+
+        next()
+    }
+    
+}
+
+module.exports = TaskValidation
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router() //identificar as rotas que estao chegando
 
 const TaskController = require('../controller/TaskController')
+const TaskValidation = require('../middleware/TaskValidation')
 
-router.post('/', TaskController.create) //rota por onde chegarao as novas tasks
+router.post('/', TaskValidation, TaskController.create) //rota por onde chegarao as novas tasks
 
 module.exports = router;
